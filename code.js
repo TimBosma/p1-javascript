14c5cfc52db60e77506e4ef8f589482b9f80226b
TimBosma <144031504+TimBosma@users.noreply.github.com>
2023-10-11 14:10:36 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,22 @@
+function calculate(getal1, getal2) 
+{
+antwoord = getal1 + getal2;
+console.log("Het Antwoord van de som is: ");
+console.log(antwoord);
+}
+
+
+function Keer5(getal1)
+{
+    antwoord = getal1 * 5;
+    console.log("het antwoord is: ");
+    console.log(antwoord);
+}
+
+function CalculateMinutes(Seconden)
+{
+    antwoord = Seconden / 60;
+    console.log("het antwoord van de som is: ");
+    console.log(antwoord)
+}
+
